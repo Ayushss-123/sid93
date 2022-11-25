068c60338a4a21aa82488f281e4d538494f43b00
ayush ayusha <ayushsrivastav323@gmail.com>
2022-11-25 08:34:49 +0000

    new commit ayusha

---
+++ pages/FrameComponent.tsx
@@ -0,0 +1,240 @@
+import type { NextPage } from "next";
+import styles from "../styles/FrameComponent.module.css";
+
+const FrameComponent: NextPage = () => {
+  return (
+    <div className={styles.frameDiv}>
+      <img className={styles.vectorIcon} alt="" src="vector-12.svg" />
+      <img className={styles.ellipseIcon} alt="" src="ellipse-16.svg" />
+      <img className={styles.vectorIcon1} alt="" src="vector-101.svg" />
+      <img className={styles.ellipseIcon1} alt="" src="ellipse-13.svg" />
+      <img className={styles.vectorIcon2} alt="" src="vector-6.svg" />
+      <div className={styles.rectangleDiv} />
+      <div className={styles.rectangleDiv1} />
+      <img className={styles.vectorIcon3} alt="" src="vector-5.svg" />
+      <img className={styles.ellipseIcon2} alt="" src="ellipse-11.svg" />
+      <img className={styles.vectorIcon4} alt="" src="vector-4.svg" />
+      <div className={styles.homeAboutUsServicesContact}>
+        <span className={styles.homeSpan}>Home</span>
+        <span> About Us Services Contact Us</span>
+      </div>
+      <div className={styles.dreamAgencyDiv}>Dream Agency</div>
+      <img className={styles.vectorIcon5} alt="" src="vector-31.svg" />
+      <img className={styles.maskGroupIcon} alt="" src="mask-group.svg" />
+      <img className={styles.vectorIcon6} alt="" src="vector-91.svg" />
+      <img className={styles.maskGroupIcon1} alt="" src="mask-group1.svg" />
+      <div className={styles.weHelpYouToGrowYourBusin}>
+        <p className={styles.weHelpYouToGrowYour}>
+          <b className={styles.weHelpYou}>We Help you </b>
+          <span className={styles.toGrowYour}>to grow your</span>
+        </p>
+        <p className={styles.businessP}>
+          <span className={styles.toGrowYour}>Business</span>
+        </p>
+      </div>
+      <div className={styles.loremIpsumHasBeenTheIndus}>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book.
+      </div>
+      <img className={styles.groupIcon} alt="" src="group-56959.svg" />
+      <img className={styles.groupIcon1} alt="" src="group-56960.svg" />
+      <img className={styles.groupIcon2} alt="" src="group-56961.svg" />
+      <div className={styles.whatWeDoForYourBusiness}>
+        <p className={styles.weHelpYouToGrowYour}>
+          <b>What We Do</b>
+        </p>
+        <p className={styles.businessP}>
+          <span className={styles.toGrowYour}>For Your Business</span>
+        </p>
+      </div>
+      <div className={styles.rectangleDiv2} />
+      <div className={styles.groupDiv}>
+        <div className={styles.rectangleDiv3} />
+        <b className={styles.websiteDesign}>Website Design</b>
+        <div className={styles.loremIpsumHasBeenTheIndus1}>
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s.
+        </div>
+        <div className={styles.rectangleDiv4} />
+        <img
+          className={styles.website49431Icon}
+          alt=""
+          src="website4943-1.svg"
+        />
+      </div>
+      <b className={styles.webDevelopment}>Web Development</b>
+      <div className={styles.loremIpsumHasBeenTheIndus2}>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s.
+      </div>
+      <div className={styles.rectangleDiv5} />
+      <img
+        className={styles.computer68231Icon}
+        alt=""
+        src="computer6823-1.svg"
+      />
+      <div className={styles.groupDiv1}>
+        <div className={styles.rectangleDiv3} />
+        <b className={styles.websiteDesign}>Mobile APP Development</b>
+        <div className={styles.loremIpsumHasBeenTheIndus1}>
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s.
+        </div>
+        <div className={styles.rectangleDiv7} />
+        <img
+          className={styles.mobilePhone11641Icon}
+          alt=""
+          src="mobilephone1164-1.svg"
+        />
+      </div>
+      <img className={styles.groupIcon3} alt="" src="group-1.svg" />
+      <div className={styles.loremIpsumHasBeenTheIndus4}>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book.
+      </div>
+      <div className={styles.loremIpsumHasBeenTheIndus}>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book.
+      </div>
+      <div className={styles.groupDiv2}>
+        <div className={styles.rectangleDiv8} />
+        <b className={styles.getStartedB}>Get Started</b>
+      </div>
+      <div className={styles.groupDiv3}>
+        <div className={styles.groupDiv4}>
+          <div className={styles.aboutUsFakeOrReal}>
+            <p className={styles.weHelpYouToGrowYour}>
+              <b>About Us</b>
+            </p>
+            <p className={styles.businessP}>
+              <span className={styles.toGrowYour}>Fake or Real</span>
+            </p>
+          </div>
+          <div className={styles.loremIpsumHasBeenTheIndus6}>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </div>
+          <div className={styles.loremIpsumHasBeenTheIndus7}>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book.
+          </div>
+        </div>
+        <div className={styles.groupDiv5}>
+          <div className={styles.rectangleDiv8} />
+          <b className={styles.exploreMoreB}>Explore More</b>
+        </div>
+      </div>
+      <img className={styles.vectorIcon7} alt="" src="vector-11.svg" />
+      <div className={styles.ourRecentWorkByOurExperts}>
+        <p className={styles.weHelpYouToGrowYour}>
+          <b>Our Recent Work</b>
+        </p>
+        <p className={styles.businessP}>
+          <span className={styles.toGrowYour}>By Our Experts</span>
+        </p>
+      </div>
+      <div className={styles.groupDiv6}>
+        <div className={styles.rectangleDiv10} />
+        <b className={styles.uISoupB}>UI Soup</b>
+        <div className={styles.groupDiv7}>
+          <div className={styles.viewWorkDiv}>View Work</div>
+          <div className={styles.rectangleDiv11} />
+        </div>
+      </div>
+      <div className={styles.groupDiv8}>
+        <div className={styles.rectangleDiv10} />
+        <b className={styles.uISoupB}>Goldcrown Labs</b>
+        <div className={styles.viewWorkDiv1}>View Work</div>
+        <div className={styles.rectangleDiv13} />
+        <b className={styles.uISoupB}>Goldcrown Labs</b>
+        <div className={styles.groupDiv7}>
+          <div className={styles.viewWorkDiv}>View Work</div>
+          <div className={styles.rectangleDiv11} />
+        </div>
+      </div>
+      <div className={styles.ourTestimonialsWhatOurClie}>
+        <p className={styles.weHelpYouToGrowYour}>
+          <b>Our Testimonials</b>
+        </p>
+        <p className={styles.businessP}>
+          <span className={styles.toGrowYour}>What Our Clients Are Saying</span>
+        </p>
+      </div>
+      <div className={styles.groupDiv10}>
+        <div className={styles.rectangleDiv15} />
+        <img className={styles.icon} alt="" src="19399522241579780463-1.svg" />
+        <div className={styles.loremIpsumHasBeenTheIndus8}>
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries.
+        </div>
+        <img className={styles.vectorIcon8} alt="" src="vector-13.svg" />
+        <div className={styles.uISoupDiv}>UI Soup</div>
+      </div>
+      <button className={styles.groupButton}>
+        <div className={styles.rectangleDiv16} />
+        <img className={styles.icon} alt="" src="19399522241579780463-1.svg" />
+        <div className={styles.loremIpsumHasBeenTheIndus9}>
+          Lorem Ipsum has been the industry's standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries.
+        </div>
+        <img className={styles.vectorIcon8} alt="" src="vector-14.svg" />
+        <div className={styles.uISoupDiv1}>UI Soup</div>
+      </button>
+      <img className={styles.groupIcon4} alt="" src="group-1.svg" />
+      <div className={styles.frameDiv1}>
+        <div className={styles.rectangleDiv17} />
+        <div className={styles.groupDiv11}>
+          <div className={styles.dreamAgencyDiv1}>Dream Agency</div>
+          <div className={styles.loremIpsumHasBeenTheIndus10}>
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries.
+          </div>
+        </div>
+        <div className={styles.groupDiv12}>
+          <div className={styles.getInTouch}>Get In Touch</div>
+          <div className={styles.groupDiv13}>
+            <div className={styles.groupDiv14}>
+              <div className={styles.rectangleDiv18} />
+              <div className={styles.enterYourEmail}>Enter Your Email</div>
+            </div>
+            <div className={styles.groupDiv15}>
+              <div className={styles.rectangleDiv19} />
+              <div className={styles.sendDiv}>Send </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.groupDiv16}>
+          <img
+            className={styles.rectangleIcon}
+            alt=""
+            src="rectangle-42@2x.png"
+          />
+          <div className={styles.rectangleDiv20} />
+        </div>
+        <div className={styles.texasUnitedStates}>
+          <p className={styles.weHelpYouToGrowYour}>
+            <b className={styles.texasB}>Texas</b>
+            <span></span>
+          </p>
+          <p className={styles.businessP}>
+            <span>United States</span>
+          </p>
+        </div>
+        <div className={styles.viewLargerMap}>View Larger Map</div>
+      </div>
+      <img className={styles.image63Icon} alt="" src="image-63@2x.png" />
+      <img className={styles.image64Icon} alt="" src="image-64@2x.png" />
+    </div>
+  );
+};
+
+export default FrameComponent;
